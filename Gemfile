b1830240bec4c240cbec84cedd59684152223ea9
rhenvar <garrickkrol@hotmail.com>
2024-08-11 21:13:59 -0400

    etherscan client balance

--- Gemfile
+++ Gemfile
@@ -48,3 +48,5 @@ group :test do
   gem 'simplecov'
   gem 'webmock'
 end
+
+gem "typhoeus", "~> 1.4"
--- lib/token_screener/services/etherscan/client.rb
+++ lib/token_screener/services/etherscan/client.rb
@@ -5,17 +5,32 @@ module TokenScreener
     module EtherScan
       class Client
 
+        attr_reader :hydra
+
         def initialize
+          @hydra = Typhoeus::Hydra.hydra
         end
 
         # Somehow fetch createdPairs in a stream or subscription?
         # Observer
         def contracts
+        end
 
+        def balance(address)
+          client.get(etherscan_api_balance_url(address))
         end
 
         private
         def client
+          Typhoeus
+        end
+
+        def etherscan_api_url
+          "https://api.etherscan.io/api?apikey=#{ENV.fetch('ETHERSCAN_API_KEY')}"
+        end
+
+        def etherscan_api_balance_url(address)
+          "#{etherscan_api_url}&module=account&action=balance&address=#{address}&tag=latest"
         end
       end
     end
