b13fc2d621c39e42a2cd3bbb4c992947e8bf23db
rhenvar <garrickkrol@hotmail.com>
2024-08-11 20:40:41 -0400

    token_screener

---
+++ lib/token_screener/services/etherscan/client.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module TokenScreener
+  module Services
+    module EtherScan
+      class Client
+
+        def initialize
+        end
+
+        # Somehow fetch createdPairs in a stream or subscription?
+        # Observer
+        def contracts
+
+        end
+
+        private
+        def client
+        end
+      end
+    end
+  end
+end
+
