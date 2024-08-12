529c4d7cd2d70c5cc2dd2a2ab1120bce7bc17fb8
rhenvar <garrickkrol@hotmail.com>
2024-08-12 04:39:46 -0400

    adding token values

--- db/migrate/20240811074653_create_tokens.rb
+++ db/migrate/20240811074653_create_tokens.rb
@@ -4,9 +4,15 @@ class CreateTokens < ActiveRecord::Migration[7.2]
   def change
     create_table :tokens do |t|
       t.string :address, null: false
-      t.datetime :release_date, null: false
-      t.boolean :rug, default: false
+      t.datetime :pair_create_date, null: false
+      t.string :network, null: false, default: 'ethereum'
+
+      t.boolean :rugged, default: false
+      t.boolean :adequate_liquidity, default: false
+      t.boolean :adequate_market_cap, default: false
+      t.boolean :adequate_transaction_count, default: false
       t.boolean :liquidity_locked, default: false
+      t.boolean :honeypot, default: true
 
       t.timestamps
     end
