1ea814660d8d3a59a0b903ba773b9d0c687fb984
rhenvar <garrickkrol@hotmail.com>
2024-08-12 21:24:54 -0400

    dexscreener client

---
+++ lib/token_screener/services/dex_screener/client.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module TokenScreener
+  module Services
+    module DexScreener
+      class Client
+
+        def liquidity(pair)
+          response(pair).dig('pair', 'liquidity', 'usd') || 0
+        end
+
+        def fdv(pair)
+          response(pair).dig('pair', 'fdv') || 0
+        end
+
+        private
+
+        def client
+          Typhoeus
+        end
+
+        def response(pair)
+          JSON.parse client.get(pairs_url(pair)).response_body
+        end
+
+        # Can pass in a list of pairs
+        def pairs_url(pair)
+          "https://api.dexscreener.com/latest/dex/pairs/ethereum/#{pair}"
+        end
+      end
+    end
+  end
+end
+
