eed0c2ba8cd1c83d0325da3bec1126bd2b10b5b1
rhenvar <garrickkrol@hotmail.com>
2024-08-15 22:58:34 -0400

    more updates

--- lib/token_screener/pipelines/steps/token_sniffer_validation.rb
+++ lib/token_screener/pipelines/steps/token_sniffer_validation.rb
@@ -5,20 +5,17 @@ module TokenScreener
     module Steps
       class TokenSnifferValidation
         # Accept Result([contracts]) returns Result
+        # Filter some tokens, call some rugs out, pass the rest along
         def call(result)
           result.halt if result.value.empty?
 
+          rugs = []
           filtered_contracts = result.value.each_with_object([]) do |address, addresses| # Filter based on TokenSniffer results
-            # time for investigation niggers
-            #
-            # Need a way to mark a contract 'rug' if certain conditions are met
-            # Perhaps --- Result.rug? monad??? :soyface: nigger it has to make sense
-            #
             next unless token_sniffer_client.audited?(address)
 
             rug = token_sniffer_client.flagged?(address) || token_sniffer_client.risk_level(address) == 'high' || !token_sniffer_client.sellable?(address) || token_sniffer_client.rug_test_detected?(address)
             sus = token_sniffer_client.score(address) < 50 || token_sniffer_client.sus_contract_detected?(address)
-            # BUSINESS LOGI
+            # BUSINESS LOGIC
             # PASS if:
             #   score < 50
             #   sus_contract_tests?
@@ -29,11 +26,15 @@ module TokenScreener
             #   sellable? == false
             #   rug_test_detected?
 
-            rug(address) if rug
+            rugs << rug if rug
             next if rug || sus
 
             addresses << address
           end
+
+          # THIS SHOULD BE A SEPARATE JOB TBH NO CAP THINK ABOUT IT FAM
+          # OR A METAPIPELINE :SOYFACE:
+          rug(rugs)
 
           # Move values onto next step. Might be empty
           result.continue(filtered_contracts)
@@ -41,8 +42,10 @@ module TokenScreener
 
         private
 
-        def rug(address)
-          Token.rug(address)
+        def rug(addresses)
+          addresses.each do |address|
+            Token.update_all({ address: }, { rugged: true })
+          end
         end
 
         def token_sniffer_client
